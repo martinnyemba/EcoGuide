e3be030b6c363c2d0e963daaac89bfa1bda87514
martinnyemba <martinnyemba@gmail.com>
2024-09-02 14:50:14 +0200

    update the database url to - sqlite:///ecoguide.db

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or str(uuid.uuid4())
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///app.db'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///ecoguide.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
