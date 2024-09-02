1159413030e346f8a691dfe03ab4b3f89c142440
martinnyemba <martinnyemba@gmail.com>
2024-09-02 14:40:11 +0200

    Added error handlers in the __init__ file

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,6 @@
-from flask import Flask
+import jinja2
+from flask import Flask, render_template
+from flask_bcrypt import Bcrypt
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
@@ -9,7 +11,9 @@ db = SQLAlchemy()
 migrate = Migrate()
 login = LoginManager()
 login.login_view = 'auth.login'
+login.login_message_category = 'info'
 mail = Mail()
+bcrypt = Bcrypt()
 
 
 def create_app(config_class=Config):
@@ -18,8 +22,30 @@ def create_app(config_class=Config):
 
     db.init_app(app)
     migrate.init_app(app, db)
+    bcrypt.init_app(app)
     login.init_app(app)
     mail.init_app(app)
+
+    # Define the error handler for TemplateNotFound
+    @app.errorhandler(jinja2.exceptions.TemplateNotFound)
+    def handle_template_not_found(error):
+        return render_template('errors/404.html'), 404
+
+    # Define the error handler for 404 Not Found
+    @app.errorhandler(404)
+    def not_found_error(error):
+        return render_template('errors/404.html'), 404
+
+    # Define the error handler for 500 Internal Server Error
+    @app.errorhandler(500)
+    def internal_error(error):
+        db.session.rollback()
+        return render_template('errors/500.html'), 500
+
+    # Define the error handler for 403 Forbidden
+    @app.errorhandler(403)
+    def forbidden_error(error):
+        return render_template('errors/403.html'), 403
 
     @login.user_loader
     def load_user(user_id):
