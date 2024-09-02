0d5da2a5daa31172b6ffbee9d520ed20a2294efb
martinnyemba <martinnyemba@gmail.com>
2024-09-02 14:43:44 +0200

    Updated the add app/utils.py

--- app/utils.py
+++ app/utils.py
@@ -1,11 +1,24 @@
 # app/utils.py
-from flask import abort
+from flask import abort, url_for, current_app
 from flask_login import current_user
 from functools import wraps
-from app import mail
 from flask_mail import Message
-from twilio.rest import Client
-import app
+from app import mail
+from itsdangerous import TimedSerializer as Serializer
+
+
+def send_reset_email(user):
+    token = user.get_reset_token()
+    msg = Message('Password Reset Request',
+                  sender='noreply@yourdomain.com',
+                  recipients=[user.email])
+    msg.body = f'''To reset your password, visit the following link:
+{url_for('auth.reset_token', token=token, _external=True)}
+
+If you did not make this request then simply ignore this email and no changes will be made.
+'''
+    mail.send(msg)
+
 
 
 def admin_required(f):
@@ -23,43 +36,6 @@ def send_email(subject, sender, recipients, text_body, html_body):
     msg.body = text_body
     msg.html = html_body
     mail.send(msg)
-
-
-def send_appointment_confirmation(booking):
-    send_email('Appointment Confirmation',
-               sender=app.config['ADMINS'][0],
-               recipients=[booking.user.email],
-               text_body=f'Your appointment for {booking.service.name} on {booking.date} has been booked.',
-               html_body=f'<p>Your appointment for {booking.service.name} on {booking.date} has been booked.</p>')
-
-
-def send_appointment_reminder(booking):
-    send_email('Appointment Reminder',
-               sender=app.config['ADMINS'][0],
-               recipients=[booking.user.email],
-               text_body=f'Reminder: Your appointment for {booking.service.name} is scheduled for {booking.date}.',
-               html_body=f'<p>Reminder: Your appointment for {booking.service.name} is scheduled for {booking.date}.</p>')
-
-
-def send_sms(to, body):
-    client = Client(app.config['TWILIO_ACCOUNT_SID'], app.config['TWILIO_AUTH_TOKEN'])
-    message = client.messages.create(
-        body=body,
-        from_=app.config['TWILIO_PHONE_NUMBER'],
-        to=to
-    )
-    return message.sid
-
-
-def send_appointment_sms_confirmation(booking):
-    body = f'Your appointment for {booking.service.name} on {booking.date} has been booked.'
-    send_sms(booking.user.phone_number, body)
-
-
-def generate_reference_number():
-    import random
-    import string
-    return ''.join(random.choices(string.ascii_uppercase + string.digits, k=8))
 
 
 def calculate_footprint(data):
