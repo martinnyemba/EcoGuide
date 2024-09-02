c58a97752f5fbfcfdb57bca33f98bd2c0f4460c1
martinnyemba <martinnyemba@gmail.com>
2024-09-02 14:32:04 +0200

    Fix : Errorss due to relationships errors between the models

--- app/models.py
+++ app/models.py
@@ -1,14 +1,24 @@
+#!/usr/bin/env python3
+"""Module for Database Models"""
 from app import db, login
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from datetime import datetime
+from itsdangerous import TimedSerializer as Serializer
+from flask import current_app
+
+@login.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 class Role(db.Model):
+    __tablename__ = 'roles'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True)
     users = db.relationship('User', backref='role', lazy='dynamic')
 
 class User(UserMixin, db.Model):
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
@@ -16,8 +26,17 @@ class User(UserMixin, db.Model):
     first_name = db.Column(db.String(64))
     last_name = db.Column(db.String(64))
     phone_number = db.Column(db.String(20))
-    role_id = db.Column(db.Integer, db.ForeignKey('role.id'))
+    reset_token = db.Column(db.String(100))
+    reset_token_expiration = db.Column(db.DateTime)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
     activities = db.relationship('Activity', backref='user', lazy='dynamic')
+    recommendations = db.relationship('Recommendation', backref='user', lazy=True)
+    user_challenges = db.relationship('UserChallenges', backref='user', lazy=True)
+    carbon_footprint = db.relationship('CarbonFootprint', backref='user', uselist=False)
+    address = db.relationship('Address', backref='user', uselist=False)
+
+    def __repr__(self):
+        return f'<User {self.username}, Email: {self.email}, Name: {self.first_name} {self.last_name}>'
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
@@ -25,30 +44,47 @@ class User(UserMixin, db.Model):
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
 
+    def get_reset_token(self, expires_sec=1800):
+        s = Serializer(current_app.config['SECRET_KEY'], expires_sec)
+        return s.dumps({'user_id': self.id}).decode('utf-8')
+
+    @staticmethod
+    def verify_reset_token(token):
+        s = Serializer(current_app.config['SECRET_KEY'])
+        try:
+            user_id = s.loads(token)['user_id']
+        except:
+            return None
+        return User.query.get(user_id)
 
 class Address(db.Model):
+    __tablename__ = 'addresses'
     id = db.Column(db.Integer, primary_key=True)
     street = db.Column(db.String(128))
     city = db.Column(db.String(64))
     state = db.Column(db.String(64))
     country = db.Column(db.String(64))
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
 
 class Activity(db.Model):
+    __tablename__ = 'activities'
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     activity_type = db.Column(db.String(20))
     description = db.Column(db.String(200))
     carbon_impact = db.Column(db.Float)
     date = db.Column(db.Date, default=datetime.utcnow)
 
 class Recommendation(db.Model):
+    __tablename__ = 'recommendations'
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100))
     description = db.Column(db.String(500))
     impact = db.Column(db.Float)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
 
 class Contact(db.Model):
+    __tablename__ = 'contacts'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), index=True)
     email = db.Column(db.String(120), index=True)
@@ -58,4 +94,21 @@ class Contact(db.Model):
     def __repr__(self):
         return f'<Contact {self.name}>'
 
+class UserChallenges(db.Model):
+    __tablename__ = 'user_challenges'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    challenge_id = db.Column(db.Integer, db.ForeignKey('eco_challenges.id'))
+    eco_challenge = db.relationship('EcoChallenges', backref='user_challenges', lazy=True)
 
+class CarbonFootprint(db.Model):
+    __tablename__ = 'carbon_footprints'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    footprint = db.Column(db.Float)
+
+class EcoChallenges(db.Model):
+    __tablename__ = 'eco_challenges'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80))
+    description = db.Column(db.Text)
